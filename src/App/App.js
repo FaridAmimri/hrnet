fac737eb01484e033104965ff0c4125bf6eb4c69
Farid Amimri <farid.amimri@gmail.com>
2022-05-07 14:53:26 +0000

    init project

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+/** @format */
+
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from '../components/Header'
+import EmployeeForm from '../pages/EmployeeForm'
+import Employees from '../pages/Employees'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<EmployeeForm />} />
+          <Route path='/employees' element={<Employees />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
