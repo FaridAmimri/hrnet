9e5e2d1e632a10f89537eaadb35c9b576259aead
Farid Amimri <farid.amimri@gmail.com>
2022-05-14 19:46:41 +0100

    Ajout gestion data LocalSorage

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -69,3 +69,36 @@ export const getStatesCollection = () => [
   { id: 58, title: 'Wisconsin' },
   { id: 59, title: 'Wyoming' }
 ]
+
+const KEYS = {
+  employees: 'employees',
+  employeeId: 'employeeId'
+}
+
+export function insertEmployee(data) {
+  let employees = getAllEmployees()
+  data['id'] = generateEmployeeId
+  employees.push(data)
+  localStorage.setItem(KEYS.employees, JSON.stringify(employees))
+}
+
+export function generateEmployeeId() {
+  if (localStorage.getItem(KEYS.employees) == null)
+    localStorage.setItem(KEYS.employeeId, '0')
+  var id = parseInt(localStorage.getItem(KEYS.employeeId))
+  localStorage.setItem(KEYS.employeeId, (++id).toString())
+  return id
+}
+
+export function getAllEmployees() {
+  if (localStorage.getItem(KEYS.employees) == null)
+    localStorage.setItem(KEYS.employees, JSON.stringify([]))
+  return JSON.parse(localStorage.getItem(KEYS.employees))
+  // let departments = getDepartmentCollection()
+  // let states = getStatesCollection()
+  // return employees.map((employee) => ({
+  //   employee
+  // department: departments[employee.departmentId - 1].title,
+  // state: states[employee.state - 1].title
+  // }))
+}
