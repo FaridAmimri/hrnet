2e74254928e0fadc3b228a83f224a6f7464332db
Farid Amimri <farid.amimri@gmail.com>
2022-05-23 19:44:39 +0100

    suppr const TableHead

--- src/utils/useTable.jsx
+++ src/utils/useTable.jsx
@@ -1,44 +1,17 @@
 /** @format */
 
-import { Table, TableHead, TableRow, TableCell } from '@mui/material'
 import styled from 'styled-components'
 
-export function useTable(records, headCells) {
-
-  const TableHeader = (props) => {
-    return (
-      <TableHead>
-        <TableRow>
-          {headCells.map((headCell) => (
-            <TableCell key={headCell.id}>{headCell.label}</TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-    )
-  }
-
-
-  const TableContainer = (props) => {
-    return <Table>{props.children}</Table>
-  }
-
-  return {
-    TableHeader,
-    TableContainer  }
-}
-
-export function MuiTable(props) {
-  const { children, ...other } = props
-
-  return (
-    <TableHeadWrapper {...other}>
-      {props.children}
-    </TableHeadWrapper>
-  )
+export function MuiTable({ children, ...other }) {
+  return <TableHeadWrapper {...other}>{children}</TableHeadWrapper>
 }
 
 const TableHeadWrapper = styled.div`
   .css-15wwp11-MuiTableHead-root {
     background-color: #0000ff24;
+  }
+  .css-1u3bzj6-MuiFormControl-root-MuiTextField-root {
+    width: 75%;
+    margin: 2% 0;
   }
 `
