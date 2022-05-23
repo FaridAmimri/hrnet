fcf46ef721a204a4f227a111310670347ad391b9
Farid Amimri <farid.amimri@gmail.com>
2022-05-23 19:26:52 +0100

    Ajout state records

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,25 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import EmployeeForm from '../pages/EmployeeForm'
-import Employees from '../pages/Employees'
+import Employees from '../pages/EmployeeTable'
+import { useState } from 'react'
+// import { useForm } from '../utils/useForm'
+import * as employeeServices from '../services/employeeServices'
 
 function App() {
+
+  // const {
+  //   values,
+  // } = useForm()
+
+  const [records, setRecords] = useState(employeeServices.getAllEmployees())
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<EmployeeForm />} />
-          <Route path='/employees' element={<Employees />} />
+          <Route path='/employees' element={<Employees />} data={records} />
         </Routes>
       </BrowserRouter>
     </>
