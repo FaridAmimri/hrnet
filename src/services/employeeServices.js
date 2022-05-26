9e563ec04f5c2771a48eb3807fa3955d6a78cbf7
Farid Amimri <farid.amimri@gmail.com>
2022-05-26 10:09:41 +0100

    Ajout fn pour générer Id employé

--- src/services/employeeServices.js
+++ src/services/employeeServices.js
@@ -70,35 +70,45 @@ export const getStateCollection = () => [
   { id: 59, title: 'Wyoming' }
 ]
 
-const KEYS = {
-  employees: 'employees',
-  employeeId: 'employeeId'
-}
-
-export function insertEmployee(data) {
-  let employees = getAllEmployees()
-  data['id'] = generateEmployeeId()
-  employees.push(data)
-  localStorage.setItem(KEYS.employees, JSON.stringify(employees))
-}
-
 export function generateEmployeeId() {
-  if (localStorage.getItem(KEYS.employeeId) == null)
-    localStorage.setItem(KEYS.employeeId, '0')
-  var id = parseInt(localStorage.getItem(KEYS.employeeId))
-  localStorage.setItem(KEYS.employeeId, (++id).toString())
+  if (localStorage.getItem('employeeId') == null)
+    localStorage.setItem('employeeId', '0')
+  var id = parseInt(localStorage.getItem('employeeId'))
+  localStorage.setItem('employeeId', (++id).toString())
   return id
 }
 
-export function getAllEmployees() {
-  if (localStorage.getItem(KEYS.employees) == null)
-    localStorage.setItem(KEYS.employees, JSON.stringify([]))
-  let employees = JSON.parse(localStorage.getItem(KEYS.employees))
-  let departments = getDepartmentCollection()
-  let states = getStateCollection()
-  return employees.map((employee) => ({
-    ...employee,
-  department: departments[employee.departmentId - 1].title,
-  state: states[employee.stateId - 1].title
-  }))
-}
+// In case of using localStorage
+
+// const KEYS = {
+//   employees: 'employees',
+//   employeeId: 'employeeId'
+// }
+
+// export function insertEmployee(data) {
+//   let employees = getAllEmployees()
+//   data['id'] = generateEmployeeId()
+//   employees.push(data)
+//   localStorage.setItem(KEYS.employees, JSON.stringify(employees))
+// }
+
+// export function generateEmployeeId() {
+//   if (localStorage.getItem(KEYS.employeeId) == null)
+//     localStorage.setItem(KEYS.employeeId, '0')
+//   var id = parseInt(localStorage.getItem(KEYS.employeeId))
+//   localStorage.setItem(KEYS.employeeId, (++id).toString())
+//   return id
+// }
+
+// export function getAllEmployees() {
+//   if (localStorage.getItem(KEYS.employees) == null)
+//     localStorage.setItem(KEYS.employees, JSON.stringify([]))
+//   let employees = JSON.parse(localStorage.getItem(KEYS.employees))
+//   let departments = getDepartmentCollection()
+//   let states = getStateCollection()
+//   return employees.map((employee) => ({
+//     ...employee,
+//   department: departments[employee.departmentId - 1].title,
+//   state: states[employee.stateId - 1].title
+//   }))
+// }
